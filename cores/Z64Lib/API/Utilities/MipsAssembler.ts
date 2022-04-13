4f70d8387c07d47f16e162162e61f0ed9cd3863a
den <denvstheworld@gmail.com>
2022-04-13 18:40:25 -0400

    Mips Assembler Helper

---
+++ cores/Z64Lib/API/Utilities/MipsAssembler.ts
@@ -0,0 +1,9 @@
+import { assemble } from "mips-assembler";
+
+export default class MipsAssembler{
+
+    static assemble(code: string): Buffer{
+        return Buffer.from(assemble(code));
+    }
+
+}
