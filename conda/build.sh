ea7c276032c3f678d0d0647e25e2c278b62e5908
den <denvstheworld@gmail.com>
2023-01-02 18:51:07 -0500

    Update build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -1,4 +1,4 @@
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/bin/client
 mkdir -p $PREFIX/bin/client/cores
-cp -r ./Z64Lib $PREFIX/bin/client/cores
+cp ./Z64Lib/Z64Lib.asar $PREFIX/bin/client/cores/Z64Lib.asar
